aee02a97ece247a2f5b0b6b2eb13d03e8beb9f68
mp2003 <m.p6092003@gmail.com>
2023-07-23 22:29:58 +0530

    it was sunday :-)

--- src/component/CardLayout.jsx
+++ src/component/CardLayout.jsx
@@ -1,9 +1,169 @@
-import React from 'react'
-
+import React from "react";
+import "./CardLayout.css";
+import { NavLink } from "react-router-dom";
+import imagePaths from "./ImageLoader.js";
+import banner1 from "../asset/banner1.jpg";
 const CardLayout = () => {
   return (
-    <div>CardLayout</div>
-  )
-}
+    <div className="card-container">
+      <div className="Layout">
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              
+            </div>
+          <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink></div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 60% off | Styles for men</span>
+            </h2>
+            <img src={imagePaths.cloths} alt="" />
+            <NavLink to="#" className="hyperlink">
+              See all offers
+            </NavLink>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="inner-box">
+            <h2>
+              <span className="headline">Up to 70% off | Clearance store</span>
+            </h2>
+            <div className="inner-box image">
+              <img src={imagePaths.box1} alt="box-image" />
+              <NavLink to="#" className="hyperlink">
+                See more
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CardLayout
+export default CardLayout;
---
+++ src/component/ImageLoader.js
@@ -0,0 +1,19 @@
+// ImageLoader.js
+import banner1 from "../asset/box-image-1.jpg";
+import bags from "../asset/bags & luggage.jpg";
+import cloths from "../asset/clothing.jpg";
+import footwear from "../asset/footwear.jpg";
+import watches from "../asset/watches.jpg"
+
+
+
+
+const imagePaths = {
+  box1: banner1,
+  bags: bags,
+    cloths: cloths,
+    footwear: footwear,
+    watches: watches
+};
+
+export default imagePaths;
